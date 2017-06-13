86f6f18973cfe736bcf1e51536838d6c7dc0c439
Strupinskiy <IStrupin@russellinvestments.com>
2017-06-13 16:15:02 -0700

    code snippets to figure out later

--- Flights.Web/Flights.Web/App/Controllers/FlightSearchController.js
+++ Flights.Web/Flights.Web/App/Controllers/FlightSearchController.js
@@ -11,4 +11,23 @@
         });
     };
 
+    //this goes in what consumed service
+    // option 2 - promises (kind of) 
+    //var res = validationResultService.getValidationResults();
+    //console.log("promised result is:", res) // res is the promise and "thenable"
+    //res.then(function (data) {
+    //    $scope.validationResults = data;
+    //});
+    //this goes in service
+    // options 2 promises
+    //var getValidationResults = function () {
+    //    var def = $q.defer()
+    //    dataService.getValidationResults.query({}, function (res) {
+    //        def.resolve(res);
+    //        console.log("res is", res);
+    //    });
+    //    return def.promise;
+    //}
+
+
 }]);
